fefbcc3111025802b4a8df7eca14d95db4ce5756
Arnav Balpande <113230128+arnavhax@users.noreply.github.com>
2025-02-13 19:39:08 +0530

    Update Program.cs

--- datatypesAndTypecastingAssignment/Program.cs
+++ datatypesAndTypecastingAssignment/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace datatypesAndTypecastingAssignment
 {
